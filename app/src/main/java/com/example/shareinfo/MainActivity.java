0131b15beeba19fa2f30366a9264292f03d9bd02
Mohamed Oulal <oulalm@wit.edu>
2021-06-30 12:24:41 -0400

    Uncommitted changes before Checkout at 6/30/2021 11:17 AM [Default Changelist]

--- app/src/main/java/com/example/shareinfo/MainActivity.java
+++ app/src/main/java/com/example/shareinfo/MainActivity.java
@@ -4,8 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +29,18 @@ public class MainActivity extends AppCompatActivity {
         Context context = getBaseContext();
         Button getInfoButton = findViewById(R.id.getInfoButton);
 
+
         getInfoButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                FetchData process = new FetchData(context);
-                process.execute();
+                String stockSymbol = "TSLA";
+                StockData.GetGeneralStockData(context);
+                StockData.GetStockData(context, stockSymbol);
+                GoogleNewsData.GetGoogleNewsDataForStock(context, stockSymbol);
+                TwitterData.GetTwitterDataForStock(context, stockSymbol);
+                RedditData.GetRedditDataForStock(context, stockSymbol);
             }
         });
     }
+
+
 }
