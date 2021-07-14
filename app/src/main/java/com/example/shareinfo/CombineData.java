baae05936d51cf44bedc0e187a058d90d43cfbfa
Mohamed Oulal <oulalm@wit.edu>
2021-07-14 12:51:31 -0400

    Finished function to combine all the different social medias and google news data files to one file.

---
+++ app/src/main/java/com/example/shareinfo/CombineData.java
@@ -0,0 +1,10 @@
+package com.example.shareinfo;
+
+public class CombineData {
+    public static void CombineAndSaveData() {
+        String mediaSource = "";
+        String userOrNetwork = "";
+        String content = "";
+        int rating = 0;
+    }
+}
