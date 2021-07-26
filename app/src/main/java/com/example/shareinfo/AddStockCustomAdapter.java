437d7e2c715bde6f03bf2ea467545a149fbe2722
Mohamed Oulal <oulalm@wit.edu>
2021-07-26 13:39:48 -0400

    Implemented the trending, personzlied, search and profile pages. With still more work to do in terms of graph creation and general functionality.

---
+++ app/src/main/java/com/example/shareinfo/AddStockCustomAdapter.java
@@ -0,0 +1,64 @@
+package com.example.shareinfo;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+
+import com.example.shareinfo.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StockTrendingCustomAdapter extends BaseAdapter {
+    private Context context;
+    private List<String> stockSymbolsList;
+    private List<String> stockNamesList;
+
+    StockTrendingCustomAdapter(Context context, List<String> stockSymbols, List<String> stockNames) {
+        this.context = context;
+        this.stockSymbolsList = stockSymbols;
+        this.stockNamesList = stockNames;
+    }
+
+    @Override
+    public int getCount() {
+        return stockSymbolsList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return this.stockSymbolsList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return this.stockSymbolsList.size();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        @SuppressLint("ViewHolder") View view = View.inflate(this.context, R.layout.add_stock_list_view, null);
+        TextView stockSymbolText = view.findViewById(R.id.stockSymbolText);
+        TextView stockNameText = view.findViewById(R.id.stockNameText);
+
+        String stockSymbol = stockSymbolsList.get(position);
+        String stockName = stockNamesList.get(position);
+        stockSymbolText.setText(stockSymbol);
+        stockNameText.setText(stockName);
+
+        ImageButton addStockButton = view.findViewById(R.id.addStockButton);
+        addStockButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Setup.AddToFavorites(context, stockSymbol, stockName);
+            }
+        });
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/example/shareinfo/MediaInformationCustomAdapter.java
@@ -0,0 +1,2 @@
+package com.example.shareinfo;public class MediaInformationCustomAdapter {
+}
